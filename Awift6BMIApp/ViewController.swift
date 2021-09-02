877366a996c0b370a8681393eaf0c47e2adaf133
hirokaissh <hirokai.saito1014@gmail.com>
2021-09-02 18:20:21 +0900

    Initial Commit

---
+++ Awift6BMIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Awift6BMIApp
+//
+//  Created by HiroakiSaito on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
